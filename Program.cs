c55d4b7966749347356badd41bba54010315e26a
kenwaynebrand <kbrand240@gmail.com>
2023-02-15 12:17:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Fizzbang fb = new Fizzbang();
+            Console.Write(fb.Start(100));
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ fizzbang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Fizzbang
+    {
+        public Fizzbang()
+        {
+
+        }
+
+        public string Start(int upper)
+        {
+            int foo = 0;
+            string res = "";
+
+            while (foo < upper)
+            {
+                bool FizzBang = false;
+                foo++;
+                if (foo % 3 == 0.0) { res += "fizz "; FizzBang = true; }
+                if (foo % 5 == 0.0) { res += "bang "; FizzBang = true; }
+                if (!FizzBang) { res += foo.ToString(); }
+                if (foo != upper) { res += Environment.NewLine; }
+            }
+            return res;
+        }
+
+    }
+}
